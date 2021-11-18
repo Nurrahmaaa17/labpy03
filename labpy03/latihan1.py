d97e9640adb1e9d82e7b22311e9ed9779bbda819
Nurrahmaaa17 <92908219+Nurrahmaaa17@users.noreply.github.com>
2021-11-19 00:28:14 +0700

    Delete

--- labpy03/latihan1.py
+++
@@ -1,7 +0,0 @@
-import random
-
-n = int(input('Masukan Nilai N: '))
-
-for i in range(n):
-    angka = random.uniform(.0, .5)
-    print("data ke-", i, ":", angka)
--- labpy03/latihan2.py
+++
@@ -1,8 +0,0 @@
-max1 = 0
-while True:
-    angka = int(input("Masukan bilangan = "))
-    if max1 < angka:
-        max1 = angka
-    if angka == 0:
-        break
-print("Bilangan Terbesarnya Adalah = ", angka)
